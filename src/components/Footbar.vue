3d2f6215fe39361d45d9a3ddb257d8af830c29c9
Zulkariski Mauladi <74296102+zulkamaula@users.noreply.github.com>
2022-05-30 02:45:01 +0000

    add responsive layout mobile and medium screen (laptop, talbet, monitor medium screen, etc.)

--- src/components/Footbar.vue
+++ src/components/Footbar.vue
@@ -1,22 +1,32 @@
 <template>
   <div class="col bg-dark p-4 text-center text-white">
     <div class="row text-footer justify-content-center">
-      <div class="col-3">
-        <div class="row justify-content-center">
-          <div class="col-2">
+      <div class="col-2 col-lg-3">
+        <div class="row justify-content-center align-items-center">
+          <div class="col-12 col-lg-2 p-2">
             <img alt="PhoneSell logo" src="../assets/images/ig.png" />
           </div>
-          <div class="col-2">
+          <div class="col-12 col-lg-2 p-2">
             <img alt="PhoneSell logo" src="../assets/images/fb.png" />
           </div>
-          <div class="col-2">
+          <div class="col-12 col-lg-2 p-2">
             <img alt="PhoneSell logo" src="../assets/images/twt.png" />
           </div>
         </div>
       </div>
-      <div class="col-7 text-right">
-        Terms & Condition &emsp; | &emsp; Copyright © 2018. All rights reserved.
-        PT Radya Gita Bahagi
+      <div class="col-10 col-lg-7 p-2 text-right align-self-center">
+        <div class="row justify-content-end">
+          <div class="col-12 col-lg-3">
+            Terms & Condition
+          </div>
+          <div class="col-1 p-0">
+            |
+          </div>
+          <div class="col-11 col-lg-7">
+            Copyright © 2018. All rights reserved. PT Radya Gita Bahagi
+          </div>
+        </div>
+        
       </div>
     </div>
   </div>
--- src/views/HomePhones.vue
+++ src/views/HomePhones.vue
@@ -1,5 +1,5 @@
 <template>
-  <div name="homephones" class="col" style="height: 250vh">
+  <div name="homephones" class="col pb-5 mb-5" style="height: auto">
     <!-- <div> -->
     <div class="container-fluid pt-3">
       <div class="row justify-content-center pl-0 mb-4">
@@ -15,17 +15,49 @@
       <div class="row justify-content-center">
         <div class="col-11">
           <div class="row justify-content-around">
+            <!-- SET FILTER -->
             <div
-              class="col-2 text-left border-bottom title-section p-0 pt-2 pb-3"
+              class="col-12 col-lg-2"
             >
-              Filter
+              <div class="row text-left border-bottom title-section p-0 pt-3 pb-3">
+                Filter
+              </div>
+
+              <div class="row mt-3">
+                <div
+                  class="col-12 pl-4 pr-4 pt-3 pb-3 border rounded text-filter"
+                  style="height: 110px"
+                >
+                  <div class="row d-flex justify-content-between mb-4">
+                    Rating 4 ke atas
+    
+                    <b-form-checkbox
+                      v-model="selected"
+                      value="inRating"
+                    ></b-form-checkbox>
+                  </div>
+    
+                  <div class="row d-flex justify-content-between mt-3">
+                    Stock Tersedia
+    
+                    <b-form-checkbox
+                      v-model="selected"
+                      value="inStock"
+                    ></b-form-checkbox>
+                  </div>
+                </div>
+              </div>
+              
             </div>
-            <div class="col-9 text-left border-bottom title-section p-0 pb-3">
-              <div class="row justify-content-between align-items-center">
-                <div class="col-3">Product List</div>
-                <div class="col-4">
+
+            <!-- CONTENT PRODUCT LIST -->
+            <div class="col-12 mt-4 mt-lg-1 col-lg-9">
+              <!-- HEADER -->
+              <div class="row text-left border-bottom justify-content-between align-items-center pb-3">
+                <div class="col-6 col-lg-3  title-section p-0">Product List</div>
+                <div class="col-6 col-lg-4">
                   <div class="row text-btn align-items-center gap-2">
-                    <div class="col-4">Urutkan</div>
+                    <div class="col-4 col-lg-3 p-0">Urutkan</div>
                     <div class="col p-2 border rounded-pill text-center mr-3">
                       Terbaru &nbsp;
                       <img src="../assets/images/Polygon 1.png" height="8" />
@@ -33,205 +65,179 @@
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
-
-          <div class="row justify-content-around mt-3 text-left">
-            <div
-              class="col-2 pl-4 pr-4 pt-3 pb-3 border rounded text-filter"
-              style="height: 110px"
-            >
-              <div class="row d-flex justify-content-between mb-4">
-                Rating 4 ke atas
-
-                <b-form-checkbox
-                  v-model="selected"
-                  value="inRating"
-                ></b-form-checkbox>
-              </div>
-
-              <div class="row d-flex justify-content-between mt-3">
-                Stock Tersedia
-
-                <b-form-checkbox
-                  v-model="selected"
-                  value="inStock"
-                ></b-form-checkbox>
-              </div>
-            </div>
-            <div class="col-9">
-              <div
-                v-if="loading == true"
-                class="row justify-content-center text-center"
-                style="height: 50vh"
-              >
-                <div class="col align-self-center">
-                  <div class="spinner-border spinner-border-sm"></div>
-                  &nbsp; Data sedang dimuat..
-                </div>
-              </div>
-
-              <div
-                v-if="phonesData.length > 0 && loading === false"
-                class="row justify-content-between"
-              >
-                <!-- Card -->
-                <div
-                  class="col-3 mr-1 mb-3 card-product"
-                  v-for="product in phonesData"
-                  :key="product.index"
-                >
-                  <div class="badges">
-                    <img
-                      v-if="product.attributes.isNew == 1"
-                      src="../assets/images/Group 3037.png"
-                    />
-                    <img
-                      v-else-if="
-                        product.attributes.rating >= 4 &&
-                        product.attributes.numOfReviews > 25
-                      "
-                      src="../assets/images/Group 3030.png"
-                    />
-                    <img
-                      v-else-if="
-                        product.attributes.isNew === 1 &&
-                        product.attributes.rating >= 4 &&
-                        product.attributes.numOfReviews > 25
-                      "
-                      src="../assets/images/Group 3036.png"
-                    />
-                  </div>
-
+              <div class="row justify-content-around mt-3 text-left">
+                <div class="col-12">
                   <div
-                    class="row p-4 text-success"
-                    v-if="product.attributes.stock > 5"
+                    v-if="loading == true"
+                    class="row justify-content-center text-center"
+                    style="height: 50vh"
                   >
-                    In Stock
+                    <div class="col align-self-center">
+                      <div class="spinner-border spinner-border-sm"></div>
+                      &nbsp; Data sedang dimuat..
+                    </div>
                   </div>
+    
                   <div
-                    class="row p-4 text-danger"
-                    v-else-if="product.attributes.stock < 5"
+                    v-if="phonesData.length > 0 && loading === false"
+                    class="row justify-content-between"
                   >
-                    Stock < 5
-                  </div>
-                  <div
-                    class="row p-4 text-danger"
-                    v-else-if="product.attributes.stock === 0"
-                  >
-                    Sold Out
-                  </div>
-                  <div class="row mt-3 p-2">
-                    <img
-                      class="col"
-                      :src="product.attributes.images[0]"
-                      height="170"
-                    />
-                  </div>
-                  <div class="row">
-                    <div class="col text-card">
-                      {{ product.attributes.name }}
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col text-poins">
-                      <img src="../assets/images/Group 2752.png" />
-                      &nbsp;{{ product.attributes.points }} poins
-                    </div>
-                  </div>
-                  <div class="row justify-content-center reviews-section">
-                    <div class="col-11 p-0">
-                      <span
-                        class="stars"
-                        v-if="product.attributes.rating < 3.5"
-                      >
-                        <i class="fas fa-star star-enable fa-xs ml-1"></i>
-                        <i class="fas fa-star star-enable fa-xs ml-1"></i>
-                        <i class="fas fa-star star-enable fa-xs ml-1"></i>
-                        <i class="fas fa-star fa-xs star-disable ml-1"></i>
-                        <i class="fas fa-star fa-xs star-disable ml-1"></i>
-                      </span>
-                      <span
-                        class="stars"
-                        v-else-if="
-                          product.attributes.rating >= 3.5 &&
-                          product.attributes.rating < 3.9
-                        "
-                      >
-                        <i class="fas fa-star star-enable fa-xs ml-1"></i>
-                        <i class="fas fa-star star-enable fa-xs ml-1"></i>
-                        <i class="fas fa-star-half fa-xs star-enable ml-1"></i>
-                        <i class="fas fa-star fa-xs star-disable ml-1"></i>
-                        <i class="fas fa-star fa-xs star-disable ml-1"></i>
-                      </span>
-                      <span
-                        class="stars"
-                        v-else-if="product.attributes.rating >= 3.9"
-                      >
-                        <i class="fas fa-star star-enable fa-xs ml-1"></i>
-                        <i class="fas fa-star star-enable fa-xs ml-1"></i>
-                        <i class="fas fa-star star-enable fa-xs ml-1"></i>
-                        <i class="fas fa-star fa-xs star-disable ml-1"></i>
-                        <i class="fas fa-star fa-xs star-disable ml-1"></i>
-                      </span>
-                      <span class="text-btn">
-                        {{ product.attributes.numOfReviews }} reviews
-                      </span>
-                    </div>
-                  </div>
-                  <div class="row love-section mt-2 justify-content-end">
-                    <div class="col-4 p-0">
-                      <img
-                        v-if="product.attributes.isWishlist !== 0"
-                        class="mx-auto"
-                        src="../assets/images/Group 2991.png"
-                      />
-                      <img
-                        v-else
-                        class="mx-auto"
-                        src="../assets/images/Group 3038.png"
-                      />
-                    </div>
-                  </div>
-
-                  <!-- Layer -->
-                  <div class="middle container">
+                    <!-- Card -->
                     <div
-                      class="row p-4 text-hover mb-3"
-                      v-if="product.attributes.stock > 5"
+                      class="col-12 col-lg-3 mr-1 mb-3 card-product"
+                      v-for="product in phonesData"
+                      :key="product.index"
                     >
-                      In Stock
-                    </div>
-                    <div
-                      class="row p-4 text-hover mb-3"
-                      v-else-if="product.attributes.stock < 5"
-                    >
-                      Stock < 5
-                    </div>
-                    <div class="row justify-content-center">
+                      <div class="badges">
+                        <img
+                          v-if="product.attributes.isNew == 1"
+                          src="../assets/images/Group 3037.png"
+                        />
+                        <img
+                          v-else-if="
+                            product.attributes.rating >= 4 &&
+                            product.attributes.numOfReviews > 25
+                          "
+                          src="../assets/images/Group 3030.png"
+                        />
+                        <img
+                          v-else-if="
+                            product.attributes.isNew === 1 &&
+                            product.attributes.rating >= 4 &&
+                            product.attributes.numOfReviews > 25
+                          "
+                          src="../assets/images/Group 3036.png"
+                        />
+                      </div>
+    
                       <div
-                        class="col-10 text-card-hover mt-4 mb-2 p-0 pt-5 pb-5"
+                        class="row p-4 text-success"
+                        v-if="product.attributes.stock > 5"
                       >
-                        {{ product.attributes.name }}
+                        In Stock
                       </div>
-                    </div>
-                    <div class="row justify-content-center mb-5 mt-2">
-                      <div class="col-11 p-0">
-                        <img src="../assets/images/Group 3003.png" />
+                      <div
+                        class="row p-4 text-danger"
+                        v-else-if="product.attributes.stock < 5"
+                      >
+                        Stock < 5
                       </div>
-                    </div>
-                    <div class="row justify-content-end text-start mt-5">
-                      <div class="col-5">
+                      <div
+                        class="row p-4 text-danger"
+                        v-else-if="product.attributes.stock === 0"
+                      >
+                        Sold Out
+                      </div>
+                      <div class="row mt-3 p-2">
                         <img
-                          v-if="product.attributes.isWishlist !== 0"
-                          class="mx-auto"
-                          src="../assets/images/Group 2991.png"
+                          class="col responsive-image"
+                          :src="product.attributes.images[0]"
                         />
-                        <img
-                          v-else
-                          class="mx-auto"
-                          src="../assets/images/Group 3038.png"
-                        />
+                      </div>
+                      <div class="row">
+                        <div class="col text-card">
+                          {{ product.attributes.name }}
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col text-poins">
+                          <img src="../assets/images/Group 2752.png" />
+                          &nbsp;{{ product.attributes.points }} poins
+                        </div>
+                      </div>
+                      <div class="row justify-content-center reviews-section">
+                        <div class="col-8 col-md-5 col-lg-8 p-0">
+                          <span
+                            class="stars"
+                            v-if="product.attributes.rating < 3.5"
+                          >
+                            <i class="fas fa-star star-enable fa-xs ml-1"></i>
+                            <i class="fas fa-star star-enable fa-xs ml-1"></i>
+                            <i class="fas fa-star star-enable fa-xs ml-1"></i>
+                            <i class="fas fa-star fa-xs star-disable ml-1"></i>
+                            <i class="fas fa-star fa-xs star-disable ml-1"></i>
+                          </span>
+                          <span
+                            class="stars"
+                            v-else-if="
+                              product.attributes.rating >= 3.5 &&
+                              product.attributes.rating < 3.9
+                            "
+                          >
+                            <i class="fas fa-star star-enable fa-xs ml-1"></i>
+                            <i class="fas fa-star star-enable fa-xs ml-1"></i>
+                            <i class="fas fa-star-half fa-xs star-enable ml-1"></i>
+                            <i class="fas fa-star fa-xs star-disable ml-1"></i>
+                            <i class="fas fa-star fa-xs star-disable ml-1"></i>
+                          </span>
+                          <span
+                            class="stars"
+                            v-else-if="product.attributes.rating >= 3.9"
+                          >
+                            <i class="fas fa-star star-enable fa-xs ml-1"></i>
+                            <i class="fas fa-star star-enable fa-xs ml-1"></i>
+                            <i class="fas fa-star star-enable fa-xs ml-1"></i>
+                            <i class="fas fa-star fa-xs star-disable ml-1"></i>
+                            <i class="fas fa-star fa-xs star-disable ml-1"></i>
+                          </span>
+                          <span class="text-btn">
+                            {{ product.attributes.numOfReviews }} reviews
+                          </span>
+                        </div>
+                        <div class="love-section col-3 col-md-3 col-lg-3 pb-3 pl-2">
+                          <img
+                            v-if="product.attributes.isWishlist !== 0"
+                            class="mx-auto"
+                            src="../assets/images/Group 2991.png"
+                          />
+                          <img
+                            v-else
+                            class="mx-auto"
+                            src="../assets/images/Group 3038.png"
+                          />
+                        </div>
+                      </div>
+    
+                      <!-- Layer -->
+                      <div class="middle container">
+                        <div
+                          class="row p-4 text-hover mb-3"
+                          v-if="product.attributes.stock > 5"
+                        >
+                          In Stock
+                        </div>
+                        <div
+                          class="row p-4 text-hover mb-3"
+                          v-else-if="product.attributes.stock < 5"
+                        >
+                          Stock < 5
+                        </div>
+                        <div class="row justify-content-center">
+                          <div
+                            class="col-10 text-card-hover mt-4 mb-2 p-0 pt-5 pb-5"
+                          >
+                            {{ product.attributes.name }}
+                          </div>
+                        </div>
+                        <div class="row justify-content-center mb-5 mt-2">
+                          <div class="col-11 p-0">
+                            <img src="../assets/images/Group 3003.png" />
+                          </div>
+                        </div>
+                        <div class="row justify-content-end text-start mt-5">
+                          <div class="col-5">
+                            <img
+                              v-if="product.attributes.isWishlist !== 0"
+                              class="mx-auto"
+                              src="../assets/images/Group 2991.png"
+                            />
+                            <img
+                              v-else
+                              class="mx-auto"
+                              src="../assets/images/Group 3038.png"
+                            />
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -239,6 +245,8 @@
               </div>
             </div>
           </div>
+
+          
         </div>
       </div>
     </div>
